2e6a93ab1ba1d5c4b2df9f79b0a7dae89b44a586
Zachary Cripps <zachary.cripps@griffithuni.edu.au>
2020-03-05 02:18:13 -0800

    defined test functions

--- Milestone1Tests/Milestone1Tests.swift
+++ Milestone1Tests/Milestone1Tests.swift
@@ -1,15 +1,15 @@
 //
-//  Milestone1Tests.swift
-//  Milestone1Tests
+//  Project1Tests.swift
+//  Project1Tests
 //
-//  Created by Zac Cripps on 4/3/20.
+//  Created by Zac Cripps on 2/3/20.
 //  Copyright © 2020 Zac Cripps. All rights reserved.
 //
 
 import XCTest
 @testable import Milestone1
 
-class Milestone1Tests: XCTestCase {
+class Project1Tests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,11 +19,23 @@ class Milestone1Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testModel() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let potatoName = "Kipfler"
+        let potatoWeight = "20grams"
+        let potatoScienceName = "Solanum tuberosum"
+        let potatoNutrition = "manganese, potassium and vitamin C"
+        let potatoFamily = "Sweet Potato"
+        let potato = Potato(name:potatoName, family:potatoFamily, weight:potatoWeight, scienceName: potatoScienceName, nutrition: potatoNutrition)
 
+        XCTAssert(potato.name == potatoName)
+        XCTAssert(potato.weight == potatoWeight)
+        XCTAssertTrue(potato.scienceName.count != 0)
+        XCTAssertEqual(potato.scienceName,potatoScienceName)
+        XCTAssertEqual(potato.nutrition,potatoNutrition)
+        XCTAssertEqual(potato.family,potatoFamily)
+    }
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
