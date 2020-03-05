fd30bd8e2ee90917f326655f2176a9bebae7cefc
Zachary Cripps <zachary.cripps@griffithuni.edu.au>
2020-03-05 01:43:37 -0800

    added Potato class to sceneDelegate

--- Milestone1/SceneDelegate.swift
+++ Milestone1/SceneDelegate.swift
@@ -12,6 +12,7 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    let potato = Potato(name: "Kipfler", family:"Sweet Potato", weight: "20grams", scienceName: "", nutrition: "manganese, potassium and vitamin C")
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -20,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = ContentView(potato: potato)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
