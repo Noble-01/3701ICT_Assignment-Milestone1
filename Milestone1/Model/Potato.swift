e9ae6ebd9415ec2e303209612f955cba608b8a71
Zachary Cripps <zachary.cripps@griffithuni.edu.au>
2020-03-05 00:29:02 -0800

    created Potatostruct for objects

---
+++ Milestone1/Model/Potato.swift
@@ -0,0 +1,22 @@
+//
+//  Potato.swift
+//  Milestone1
+//
+//  Created by Zac Cripps on 5/3/20.
+//  Copyright © 2020 Zac Cripps. All rights reserved.
+//
+struct Potato {
+    let name:String
+    let scienceName:String
+    let family:String
+    let weight: String
+    let nutrition:String
+
+    init(name:String, family:String, weight:String, scienceName:String, nutrition:String) {
+        self.name = name
+        self.family  = family
+        self.weight = weight
+        self.scienceName = scienceName
+        self.nutrition = nutrition
+    }
+}
